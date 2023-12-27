01e489256d180f77754c814af0b1ed1991894858
konoplyyaa1 <conopliovictorr@gmail.com>
2023-12-27 18:01:32 +0200

    feat: animation init

--- src/entrypoints/index.js
+++ src/entrypoints/index.js
@@ -1,5 +1,10 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
 import ProductForm from '../product-form'
 import GridList from '../collection-grid-slider'
 
 window.customElements.define('product-form', ProductForm)
 window.customElements.define('grid-list', GridList)
+
+AOS.init()
